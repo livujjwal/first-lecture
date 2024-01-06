727fa6dab904f625d26180fdd98edbb4c2a31493
livujjwal <ujjwalsingh2112@gamil.com>
2024-01-06 22:22:19 +0530

    react-start

---
+++ App.js
@@ -0,0 +1,25 @@
+// const heading  = React.createElement("h1",{id:"heading",a:"hello"},"Welcome to React");
+//Nesting Elements => 3rd argument use to nesting another element single or array
+
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//  [ React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", { id: "heading" }, "tag h1"),
+//     React.createElement("h2", { id: "heading" }, "tag h2"),
+//   ]), React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", { id: "heading" }, "tag h1"),
+//     React.createElement("h2", { id: "heading" }, "tag h2"),
+//   ])]
+// );
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+   React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", { id: "heading" }, "tag h1"),
+      React.createElement("h2", { id: "heading" }, "tag h2"),
+    ])
+  );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
