90dc97e2d59af4866332e80e0437a58a3c5131e3
livujjwal <ujjwalsingh2112@gamil.com>
2024-01-07 14:27:27 +0530

    2nd lect

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
-// const heading  = React.createElement("h1",{id:"heading",a:"hello"},"Welcome to React");
-//Nesting Elements => 3rd argument use to nesting another element single or array
+/*
+const heading  = React.createElement("h1",{id:"heading",a:"hello"},"Welcome to React");
+Nesting Elements => 3rd argument use to nesting another element single or array
 
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//  [ React.createElement("div", { id: "child" }, [
-//     React.createElement("h1", { id: "heading" }, "tag h1"),
-//     React.createElement("h2", { id: "heading" }, "tag h2"),
-//   ]), React.createElement("div", { id: "child2" }, [
-//     React.createElement("h1", { id: "heading" }, "tag h1"),
-//     React.createElement("h2", { id: "heading" }, "tag h2"),
-//   ])]
-// );
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+ [ React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { id: "heading" }, "tag h1"),
+    React.createElement("h2", { id: "heading" }, "tag h2"),
+  ]), React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading" }, "tag h1"),
+    React.createElement("h2", { id: "heading" }, "tag h2"),
+  ])]
+);
+*/
+import React from "react";
+import  ReactDOM  from "react-dom/client";
 const parent = React.createElement(
     "div",
     { id: "parent" },
